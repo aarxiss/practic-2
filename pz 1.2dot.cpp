4e56c0b0d1729e30286884a00b87d5737148f106
aarxiss <grecanikolay06@gmail.com>
2024-10-04 19:06:26 +0300

    pz 1.2dot

---
+++ pz 1.2dot.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "uk_UA");
+    int number1, number2;
+
+    std::cout << "Введіть перше число: ";
+    std::cin >> number1;
+    std::cout << "Введіть друге число: ";
+    std::cin >> number2;
+
+
+    int max = (number1 > number2) ? number1 : number2;
+
+    std::cout << "Більше число: " << max << std::endl;
+
+    return 0;
+}
